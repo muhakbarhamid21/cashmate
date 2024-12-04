7baf8949190b7cc157219a55474ccbb649a68c93
Muhammad Akbar Hamid <muhakbarhamid21@gmail.com>
2024-12-04 21:31:58 +0700

    feat: add colors theme

---
+++ lib/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PaletteColors {
+  static const Color color0 = Color(0xFF93C9F6); // Light Blue
+  static const Color color1 = Color(0xFF509EDF); // Blue
+  static const Color color2 = Color(0xFF2973AD); // Darker Blue
+  static const Color color3 = Color(0xFF3ACA78); // Green
+  static const Color color4 = Color(0xFFC4E6DB); // Pale Green
+  static const Color color5 = Color(0xFFCCE0EF); // Light Grey-Blue
+  static const Color color6 = Color(0xFF061E34); // Dark Navy
+}
